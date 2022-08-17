2b61e9529981feacec60bd14da330e7c8ea0b485
Helio <heliohenrique3@gmail.com>
2022-08-16 21:42:29 -0300

    Configurando para deploy

--- index.py
+++ index.py
@@ -1,24 +1,17 @@
-import datetime
 from http.client import BAD_REQUEST, NOT_FOUND, UNAUTHORIZED, UNPROCESSABLE_ENTITY
 import json
-from time import strptime
 from typing import List
 from flask import (
     Flask,
     session,
     escape,
-    make_response,
-    redirect,
     render_template,
     request,
-    url_for,
 )
-from flask_sqlalchemy import Pagination, SQLAlchemy
+from config import setup_database
 
 from model import Contato, Usuario, db
 
-import psycopg2
-import psycopg2.extras
 from flask_migrate import Migrate
 
 from flask_session import Session
@@ -32,24 +25,13 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost:5432/agenda'
+setup_database(app)
 
 db.init_app(app)
 migrate = Migrate(app, db)
 
 PER_PAGE = 10
 
-
-## Usuario 1 --> * Contato
-
-@app.get("/")
-def main():
-    return render_template("index.html", data=datetime.datetime.utcnow())
-
-
-@app.get("/about/")
-def about():
-    return render_template("about.html")
 
 @app.get("/contatos")
 def contatos_json():
