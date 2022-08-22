c4437ac5909fad3d5bc4d91ee7c0901021247c76
Helio <heliohenrique3@gmail.com>
2022-08-22 15:10:37 -0300

    Alterando versao do Python

---
+++ run_db_remote.sh
@@ -0,0 +1,12 @@
+# EXECUTAR SOMENTE NO GIT BASH OU TERMINAL DO LINUX
+
+export FLASK_APP=index
+export FLASK_ENV=development
+
+export DB_HOST=ec2-34-234-240-121.compute-1.amazonaws.com
+export DB_USER=rzpmchffeeulbf
+export DB_PASSWORD=a39320ddae53c5023c2322e22491c74513fa23db0f619aa0f9e02e6caf2ca072
+export DB_PORT=5432
+export DB_NAME=dfmrpf7seeh215
+
+flask run
---
+++ run_migration_remote.sh
@@ -0,0 +1,10 @@
+export FLASK_APP=index
+export FLASK_ENV=development
+export DB_HOST=ec2-34-234-240-121.compute-1.amazonaws.com
+export DB_USER=rzpmchffeeulbf
+export DB_PASSWORD=a39320ddae53c5023c2322e22491c74513fa23db0f619aa0f9e02e6caf2ca072
+export DB_PORT=5432
+export DB_NAME=dfmrpf7seeh215
+
+flask db migrate
+flask db upgrade
